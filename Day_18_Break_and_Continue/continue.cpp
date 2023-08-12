498b8325748f944fb3e2fa34170926eef9e7f158
Ashish Shrestha <ash.shrestha0360@gmail.com>
2023-08-12 20:01:36 +0545

    break and continue🧩

--- Day_18_Break_and_Continue/continue.cpp
+++ Day_18_Break_and_Continue/continue.cpp
@@ -1,11 +1,11 @@
-// the following loop will print upto 4 skipping the number 4 as 0 1 2 3 5.
+// the following loop will print upto 7 skipping the number 4 as 0 1 2 3 5 6 7.
 
 #include <iostream>
 using namespace std;
 
 int main()
 {
-  for (int i = 0; i < 5; i++)
+  for (int i = 0; i < 8; i++)
   {
     if (i == 4)
     {
